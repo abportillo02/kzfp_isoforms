f7097a594ac6eaf46c4197bce1b4c4870a70bc35
abportillo02 <abportillo@coh.org>
2025-10-01 11:44:46 -0700

    stingtie

--- scripts/stringtie.sh
+++ scripts/stringtie.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Check if input file with sample names is provided
+# Check for sample list
 if [ -z "$1" ]; then
   echo "Usage: $0 <sample_list.txt>"
   exit 1
@@ -9,30 +9,41 @@ fi
 samples="$1"
 bamdir=/home/abportillo/github_repo/kzfp_isoforms/fastq_files/rnaPreprocess/filtered_bams
 outdir=/home/abportillo/github_repo/kzfp_isoforms/fastq_files/rnaPreprocess/stringtie_ballgown
-gtf=/net/nfs-irwrsrchnas01/labs/dschones/bioresearch/qianhui/hg38_2024/hg38_p14/teAnno_round3/filtered_gencode_v46_chr_patch_hapl_scaff_annotation.gtf
+gtf=/path/to/gencode_or_merged.gtf
 stringtie=/home/abportillo/.conda/envs/mamba_abner_BC/bin/stringtie
 
-# Create output directory if it doesn't exist
 mkdir -p "$outdir"
 
 while IFS= read -r sample_name; do
-  echo "Running StringTie for $sample_name..."
+  echo "Creating job for $sample_name..."
 
+  job_script="${outdir}/${sample_name}_stringtie_job.sh"
   input_bam="${bamdir}/${sample_name}_sorted_q30fil_nr_sorted.bam"
   output_gtf="${outdir}/${sample_name}_Gencode_transcripts_ballgown.gtf"
   sample_outdir="${outdir}/ballgown/${sample_name}"
 
-  if [ ! -f "$input_bam" ]; then
-    echo "Missing BAM file: $input_bam"
-    continue
-  fi
-
   mkdir -p "$sample_outdir"
 
-  $stringtie "$input_bam" -e -B -p 8 \
-    -G "$gtf" \
-    -o "$output_gtf" \
-    -A "${sample_outdir}/counts.txt"
+  cat <<EOF > "$job_script"
+#!/bin/bash
+#SBATCH --job-name=StringTie_${sample_name}
+#SBATCH --output=${outdir}/${sample_name}_stringtie_%j.log
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=abportillo@coh.org
+#SBATCH -n 8
+#SBATCH -N 1
+#SBATCH -p all
+#SBATCH --mem=64G
+#SBATCH --time=24:00:00
 
-  echo "Finished $sample_name"
+source /home/abportillo/.bashrc
+conda activate /home/abportillo/.conda/envs/mamba_abner_BC
+
+$stringtie "$input_bam" -e -B -p 8 \
+  -G "$gtf" \
+  -o "$output_gtf" \
+  -A "${sample_outdir}/abundance.txt"
+EOF
+
+  sbatch "$job_script"
 done < "$samples"
