eb3f485e8c763449ed27a029cafc5b91392888b6
abportillo02 <abportillo@coh.org>
2025-09-30 17:13:35 -0700

    path/

--- scripts/sample.txt.sh
+++ scripts/sample.txt.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ## get sample names and save to a txt file
-ls /home/abportillo/github_repo/kzfp_isoforms/fastq_files*.fastq | \
+ls /home/abportillo/github_repo/kzfp_isoforms/fastq_files/*.fastq | \
 awk -F'_' '{print $1}' | \
 sort | \
 uniq > /home/abportillo/github_repo/kzfp_isoforms/scripts/sample.txt
