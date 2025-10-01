afd5c9de411a286e4666b353d8fdce1b57dd880c
abportillo02 <abportillo@coh.org>
2025-09-30 17:17:58 -0700

    sample.txt

--- scripts/sample.txt.sh
+++ scripts/sample.txt.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-## get sample names and save to a txt file
+# Get sample names from .fastq files only (excluding .bz2)
 ls /home/abportillo/github_repo/kzfp_isoforms/fastq_files/*.fastq | \
-awk -F'_' '{print $1}' | \
-sort | \
-uniq > /home/abportillo/github_repo/kzfp_isoforms/scripts/sample.txt
-
+grep -v '.bz2' | \
+awk -F'/' '{print $NF}' | \
+sed 's/\.fastq$//' | \
+sort | uniq > /home/abportillo/github_repo/kzfp_isoforms/scripts/sample.txt
 
