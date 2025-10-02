07fcf53ffcd1491a65aee75e65a82e2b73233e7c
abportillo02 <abportillo@coh.org>
2025-10-02 16:01:51 -0700

    de_novo discovery

--- scripts/stringtie.sh
+++ scripts/stringtie.sh
@@ -9,7 +9,7 @@ fi
 samples="$1"
 bamdir=/home/abportillo/github_repo/kzfp_isoforms/fastq_files/rnaPreprocess/filtered_bams
 outdir=/home/abportillo/github_repo/kzfp_isoforms/fastq_files/rnaPreprocess/stringtie_ballgown
-gtf=/net/nfs-irwrsrchnas01/labs/dschones/bioresearch/qianhui/hg38_2024/hg38_p14/teAnno_round3/filtered_gencode_v46_chr_patch_hapl_scaff_annotation.gtf
+# gtf=/net/nfs-irwrsrchnas01/labs/dschones/bioresearch/qianhui/hg38_2024/hg38_p14/teAnno_round3/filtered_gencode_v46_chr_patch_hapl_scaff_annotation.gtf
 stringtie=/home/abportillo/.conda/envs/mamba_abner_BC/bin/stringtie
 
 mkdir -p "$outdir"
@@ -18,8 +18,8 @@ while IFS= read -r sample_name; do
   echo "Creating job for $sample_name..."
 
   job_script="${outdir}/${sample_name}_stringtie_job.sh"
-  input_bam="${bamdir}/${sample_name}_sorted_q30fil_nr_sorted.bam"
-  sample_outdir="${outdir}/ballgown/${sample_name}"
+  input_bam="${bamdir}/${sample_name}_nr_sorted.bam"
+  sample_outdir="${outdir}/ballgown2/${sample_name}"
 
   mkdir -p "$sample_outdir"
 
@@ -39,8 +39,8 @@ source /home/abportillo/.bashrc
 conda activate /home/abportillo/.conda/envs/mamba_abner_BC
 
 $stringtie "$input_bam" -e -B -p 8 \
-  -G "$gtf" \
-  -o "${sample_outdir}/${sample_name}_ballgown.gtf" \
+  # -G "$gtf" \
+  -o "${sample_outdir}/${sample_name}__ballgown.gtf" \
   -A "${sample_outdir}/counts.txt"
 EOF
 
