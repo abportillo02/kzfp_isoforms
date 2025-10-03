a9fb211cc7286a51e7b6c6c5b8e5704ee675c426
abportillo02 <abportillo@coh.org>
2025-10-03 09:23:57 -0700

    novel merge

--- scripts/prepare_transcriptome.sh
+++ scripts/prepare_transcriptome.sh
@@ -13,13 +13,13 @@ set -e
 
 # Define input files
 MERGELIST="/home/abportillo/github_repo/kzfp_isoforms/scripts/mergelist.txt"
-# REFERENCE_GTF="/net/nfs-irwrsrchnas01/labs/dschones/bioresearch/qianhui/hg38_2024/hg38_p14/teAnno_round3/filtered_gencode_v46_chr_patch_hapl_scaff_annotation.gtf"
-# REFERENCE_FASTA="/home/abportillo/github_repo/RNA_seq_Bcell/output/raw_fastq_bcell/rnaPreprocess/hg38_p14/hg38_p14.fa"
+REFERENCE_GTF="/net/nfs-irwrsrchnas01/labs/dschones/bioresearch/qianhui/hg38_2024/hg38_p14/teAnno_round3/filtered_gencode_v46_chr_patch_hapl_scaff_annotation.gtf"
+REFERENCE_FASTA="/home/abportillo/github_repo/RNA_seq_Bcell/output/raw_fastq_bcell/rnaPreprocess/hg38_p14/hg38_p14.fa"
 
 # Define output directory
 OUTDIR="/home/abportillo/github_repo/kzfp_isoforms/fastq_files/rnaPreprocess"
 MERGED_GTF="${OUTDIR}/novel_merged.gtf"
-# TRANSCRIPT_FASTA="${OUTDIR}/isoform_nt.fa"
+TRANSCRIPT_FASTA="${OUTDIR}/novel_isoform_nt.fa"
 
 # Define tools
 STRINGTIE=/home/abportillo/.conda/envs/mamba_abner_BC/bin/stringtie
@@ -40,10 +40,10 @@ fi
 echo "Merging GTFs using StringTie..."
 "$STRINGTIE" --merge -G "$REFERENCE_GTF" -o "$MERGED_GTF" "$MERGELIST"
 
-# Step 2: Generate transcript FASTA
-# echo "Generating transcript FASTA using gffread..."
-# "$GFFREAD" "$MERGED_GTF" -g "$REFERENCE_FASTA" -w "$TRANSCRIPT_FASTA"
+Step 2: Generate transcript FASTA
+echo "Generating transcript FASTA using gffread..."
+"$GFFREAD" "$MERGED_GTF" -g "$REFERENCE_FASTA" -w "$TRANSCRIPT_FASTA"
 
 echo "Done. Output files:"
 echo "  - Merged GTF: $MERGED_GTF"
-# echo "  - Transcript FASTA: $TRANSCRIPT_FASTA"
+echo "  - Transcript FASTA: $TRANSCRIPT_FASTA"
