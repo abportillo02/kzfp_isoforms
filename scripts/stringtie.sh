55ef060353eb752b0bc90a028b72139bb6744f3b
abportillo02 <abportillo@coh.org>
2025-10-14 09:59:01 -0700

    stringtie 2

--- scripts/stringtie.sh
+++ scripts/stringtie.sh
@@ -7,8 +7,8 @@ if [ -z "$1" ]; then
 fi
 
 samples="$1"
-bamdir=/home/abportillo/github_repo/kzfp_isoforms/fastq_files/rnaPreprocess/Star_files
-outdir=/home/abportillo/github_repo/kzfp_isoforms/fastq_files/rnaPreprocess/stringtie_ballgown/denovo
+bamdir=/home/abportillo/github_repo/kzfp_isoforms/fastq_files/rnaPreprocess/Star_files_2
+outdir=/home/abportillo/github_repo/kzfp_isoforms/fastq_files/rnaPreprocess/stringtie_ballgown/denovo_2
 # gtf=/net/nfs-irwrsrchnas01/labs/dschones/bioresearch/qianhui/hg38_2024/hg38_p14/teAnno_round3/filtered_gencode_v46_chr_patch_hapl_scaff_annotation.gtf
 stringtie=/home/abportillo/.conda/envs/mamba_abner_BC/bin/stringtie
 
